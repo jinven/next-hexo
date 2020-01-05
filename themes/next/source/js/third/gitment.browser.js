ac2434b4af29677a6e048a12fab6aad1a559c560
a <a@A>
2020-01-05 09:27:22 +0800

    'cors'

--- themes/next/source/js/third/gitment.browser.js
+++ themes/next/source/js/third/gitment.browser.js
@@ -3413,7 +3413,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('http://localhost:3000', {
+      _utils.http.post('https://cors-server.now.sh/api/cors', {
       // _utils.http.post('https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token', {
         code: code,
         client_id: client_id,
